29342ac3558bd595ed3a569ea339c6fc55ddfbb7
Crothos <s.g.totten@gmail.com>
2023-09-07 15:03:54 -0500

    Data temp from array

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+const data = ["Sayali Deshmukh", "Fatema Hussain", "Kevin Ng", "Maharajan Rathakrishnan", "Gowtham Ravichandran", "Dharmendra Sharma", "Stephen Totten"];
+const autocomplete = document.getElementById("host");
+const resultsHTML = document.getElementById("results");
+
+autocomplete.oninput = function () {
+    let results = [];
+    const userInput = this.value;
+    resultsHTML.innerHTML = "";
+    if (userInput.length > 0) {
+      results = getResults(userInput);
+      resultsHTML.style.display = "block";
+      for (i = 0; i < results.length; i++) {
+        resultsHTML.innerHTML += "<li>" + results[i] + "</li>";
+      }
+    }
+  };
+
+  function getResults(input) {
+    const results = [];
+    for (i = 0; i < data.length; i++) {
+      if (input === data[i].slice(0, input.length)) {
+        results.push(data[i]);
+      }
+    }
+    return results;
+  }
+
+  resultsHTML.onclick = function (event) {
+    const setValue = event.target.innerText;
+    autocomplete.value = setValue;
+    this.innerHTML = "";
+  };
+
 document.addEventListener('DOMContentLoaded', function () {
+    
     const checkInForm = document.getElementById('checkInForm');
     const checkInList = document.getElementById('checkInList');
 
